cbeca88c7b0141bfd657f141102fa04d66453937
oHiago <hiagobica@gmail.com>
2025-10-09 12:16:11 +0000

    camisa

--- myrep/poo/camisa/py/draft.py
+++ myrep/poo/camisa/py/draft.py
@@ -1,20 +1,23 @@
 class Camisa:
     def __init__(self, tamanho: str):
-        self.__tamanho: int = ""
+        self.__tamanho = ""
 
     def getTamanho(self) -> str:
         return self.__tamanho
 
-    def setTamanho(self, valor: str):
+    def setTamanho(self, valor: str) -> bool:
         if not (valor == "PP" or valor == "P" or valor == "M" or valor == "G" or valor == "GG" or valor == "XG"):
             print("valor invalido, tente PP, P, M, G, GG ou XG")
+            return False
         self.__tamanho = valor
+        return True
 
-    camisa = Camisa()
+camisa = Camisa()
 
-    while roupa.getTamanho() == "":
+while camisa.getTamanho() == "":
         print("digite seu tamanho de roupa:")
         tamanho = input()
-        if roupa.setTamanho(tamanho):
+        if camisa.setTamanho(tamanho):
             break
-        print("parabens, voce comprou uma roupa tamanho", roupa.getTamanho())
+
+        print("parabens, voce comprou uma roupa tamanho", camisa.getTamanho())
